cb76bf7dce69078d83422bdf9d74eeeb9f936045
Matt Gabrenya <matt@buildyourweb.app>
2023-03-03 08:30:55 -0700

    bug reproduction: cell name modifies dna hash

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,3 @@
 CARGO_TARGET_DIR=target cargo build --release --target wasm32-unknown-unknown
 hc dna pack dnas/forum/workdir
-WASM_LOG=warn cargo test
+npm run test
--- tests/src/forum/posts/post.test.ts
+++ tests/src/forum/posts/post.test.ts
@@ -3,7 +3,7 @@ import test from "tape";
 import { runScenario, pause, CallableCell } from '@holochain/tryorama';
 import { NewEntryAction, ActionHash, Record, AppBundleSource } from '@holochain/client';
 
-test('create Post', async t => {
+test('clone cells', async t => {
   await runScenario(async scenario => {
     // Construct proper paths for your app.
     // This assumes app bundle created by the `hc app pack` command.
@@ -15,24 +15,42 @@ test('create Post', async t => {
     // Add 2 players with the test app to the Scenario. The returned players
     // can be destructured.
     const [alice, bob] = await scenario.addPlayersWithApps([appSource, appSource]);
+    const aliceAppAgent = alice.conductor.appAgentWs();
 
     // Shortcut peer discovery through gossip and register all agents in every
     // conductor of the scenario.
     await scenario.shareAllAgents();
 
-    // Alice creates a Post
+    // Alice clones the cell with a name
     try {
-      const post_ah: ActionHash = await alice.cells[0].callZome({
-        zome_name: "posts",
-        fn_name: "create_post",
-        payload: {
-          title: 'my post',
-          content: 'this is a post'
+      const cloneCell1 = await aliceAppAgent.createCloneCell({
+        role_name: 'forum',
+        modifiers: {
+          network_seed: '1234'
+        }
+      });
+      
+      const cloneCell2 = await aliceAppAgent.createCloneCell({
+        role_name: 'forum',
+        name: 'named cell A',
+        modifiers: {
+          network_seed: '1234'
         }
       });
 
-      console.log('post is ', post_ah);
-      t.ok(post_ah);
+      const cloneCell3 = await aliceAppAgent.createCloneCell({
+        role_name: 'forum',
+        name: 'named cell B',
+        modifiers: {
+          network_seed: '1234'
+        }
+      });
+      
+      t.deepEqual(cloneCell1.original_dna_hash, cloneCell2.original_dna_hash);
+      t.deepEqual(cloneCell2.original_dna_hash, cloneCell3.original_dna_hash);
+
+      t.deepEqual(cloneCell1.cell_id[0], cloneCell2.cell_id[0], "Named cell should have same DNA hash as otherwise identical unnamed cell");
+      t.deepEqual(cloneCell2.cell_id[0], cloneCell3.cell_id[0], "Named cells with different names but otherwise identical should have same DNA hashes");
     } catch(e) {
       t.fail(e.data.data);
     }
